ffdd53f6482554803bb5c8de8bfba42d383a6f68
Arda Cinar <arda@cinar.be>
2016-12-23 17:46:59 +0300

    Add issue

--- frontend/src/Model.js
+++ frontend/src/Model.js
@@ -2,9 +2,17 @@
 
 import { Record, List } from 'immutable';
 
+type IssueType = 'OPEN' | 'CLOSED' | 'DOING' | 'WONTFIX';
+
+const Issue = Record({
+    id: 0,
+    name: "",
+    issueType: 'OPEN'
+});
+
 const Model = Record({
     issues : new List(),
     numIssues : 0
 });
 
-export { Model };
+export { Issue, Model };
--- frontend/src/update/update.js
+++ frontend/src/update/update.js
@@ -1,11 +1,26 @@
 // @flow
 
-import { Model } from '../Model';
+import { Issue, Model } from '../Model';
 import { Record } from 'immutable';
 
-type Event = 'NEW_ISSUE' | 'CLOSE_ISSUE' | 'FILTER_ISSUES';
+type Event = { type: 'NEW_ISSUE', name: string }
+    | { type: 'CLOSE_ISSUE', id: number }
+    | { type: 'FILTER_ISSUES', filter: string };
+
+const createIssue = (id: number, name: string): Issue => {
+    return new Issue({ id: id, name: name, issueType: 'OPEN'});
+}
 
 const update = (prev: Record<Model>, action: Event): Record<Model> => {
+    switch(action.type) {
+        case 'NEW_ISSUE':
+        new Model({ 
+            issues: prev.get('issues').push(createIssue(prev.get('numIssues') + 1, prev.get('name'))), 
+            numIssues: prev.get('numIssues') + 1 });
+        case 'CLOSE_ISSUE':
+        return prev;
+    }
+
     return prev;
 }
 
